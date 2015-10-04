aa8aec7afab8e9dd9b09d16b9a0136d99722be8d
Zack Tollman <tollmanz@gmail.com>
2015-10-04 12:15:25 -0400

    Make mock data sharable.
    We are going to need response data in a lot of places. Let's make it
    easily sharable.

--- test/tests/DirectoryTest.php
+++ test/tests/DirectoryTest.php
@@ -14,32 +14,7 @@ class DirectoryTest extends PHPUnit_Framework_TestCase {
 			'body' => '',
 		);
 
-		$response = array(
-			'headers'  => array(
-				'server'                      => 'nginx',
-				'content-type'                => 'application/json',
-				'content-length'              => '279',
-				'access-control-allow-origin' => array(
-					'*',
-					'*',
-				),
-				'replay-nonce'                => 'Cee2LkvwXwSoMBC4nSRDQfeOzIGHb6WOEbtfpSwv4FI',
-				'x-frame-options'             => 'DENY',
-				'strict-transport-security'   => 'max-age=604800',
-				'expires'                     => 'Sun, 04 Oct 2015 14:38:51 GMT',
-				'cache-control'               => 'max-age=0, no-cache, no-store',
-				'pragma'                      => 'no-cache',
-				'date'                        => 'Sun, 04 Oct 2015 14:38:51 GMT',
-				'connection'                  => 'close',
-			),
-			'body'     => '{"new-authz":"https://acme-staging.api.letsencrypt.org/acme/new-authz","new-cert":"https://acme-staging.api.letsencrypt.org/acme/new-cert","new-reg":"https://acme-staging.api.letsencrypt.org/acme/new-reg","revoke-cert":"https://acme-staging.api.letsencrypt.org/acme/revoke-cert"}',
-			'response' => array(
-				'code'    => 200,
-				'message' => 'OK',
-			),
-			'cookies'  => array(),
-			'filename' => null,
-		);
+		$response = MockData::get_directory_response();
 
 		\WP_Mock::setUp();
 
