9fe95368372cd2d6d9dbd2bf296f81f467cd078c
Zack Tollman <tollmanz@gmail.com>
2015-10-04 12:29:46 -0400

    Allow resources to process Directory responses.
    After a Directory object has a response, the Resources object can
    process its results into a nice little list of resources that can be
    used by other objects.

--- src/Resources/Resources.php
+++ src/Resources/Resources.php
@@ -2,22 +2,96 @@
 
 namespace LEWP\Resources;
 
-/**
- * Gets resources from an ACME server via the directory resource
- */
-
 class Resources {
-	private $directory_url = '';
+	/**
+	 * List of RESTful resources for an ACME server.
+	 *
+	 * @var array
+	 */
+	private $resource_urls = array();
 
-	public function __construct( $directory_url ) {
-		$this->set_directory_url( $directory_url );
+	/**
+	 * Construct the object.
+	 *
+	 * @param  string    $directory_request_body    The response from a Directory object request response.
+	 */
+	public function __construct( $directory_request_body = '' ) {
+		if ( ! empty( $directory_request_body ) ) {
+			$resources = $this->extract_resources( $directory_request_body );
+			$this->set_resource_urls( $resources );
+		}
 	}
 
-	public function set_directory_url( $url ) {
-		$this->directory_url = $url;
+	/**
+	 * Get the resources out of a request body.
+	 *
+	 * @param  string    $directory_request_body   JSON string with resources.
+	 * @return array                               Array of sanitized resources.
+	 */
+	private function extract_resources( $directory_request_body ) {
+		$body      = json_decode( $directory_request_body, true );
+		$resources = $this->sanitize_resource_list( $body );
+
+		return $resources;
 	}
 
-	public function get_directory_url() {
-		return $this->directory_url;
+	/**
+	 * Checks a list of resources against a whitelist of resource names.
+	 *
+	 * @param  array    $resources    The list of resources with resource name as a key.
+	 * @return array                  The cleaned list of resources.
+	 */
+	private function sanitize_resource_list( $resources ) {
+		$clean_resources       = array();
+		$whitelisted_resources = $this->resource_whitelist();
+
+		foreach ( $resources as $resource_name => $resource ) {
+			if ( in_array( $resource_name, $whitelisted_resources ) ) {
+				$clean_resources[ $resource_name ] = $resource;
+			}
+		}
+
+		return $clean_resources;
+	}
+
+	/**
+	 * Acceptable resource names.
+	 *
+	 * These represent the resource "names" that the ACME protocol supports. The directory resource should return a list
+	 * of resources keyed with these values.
+	 *
+	 * @return array    List of acceptable ACME resources.
+	 */
+	private function resource_whitelist() {
+		return array(
+			'new-reg',
+			'recover-reg',
+			'new-authz',
+			'new-cert',
+			'revoke-cert',
+			'reg',
+			'authz',
+			'challenge',
+			'cert',
+		);
+	}
+
+	/**
+	 * Set the list of resources URLs.
+	 *
+	 * @param  array    $urls    A list of resource URLs.
+	 * @return void
+	 */
+	public function set_resource_urls( $urls ) {
+		$this->resource_urls = $urls;
+	}
+
+	/**
+	 * Return the list of resources.
+	 *
+	 * @return array    The list of resources.
+	 */
+	public function get_resource_urls() {
+		return $this->resource_urls;
 	}
 }
--- test/tests/ResourcesTest.php
+++ test/tests/ResourcesTest.php
@@ -1,21 +1,59 @@
 <?php
 
 class ResourcesTest extends PHPUnit_Framework_TestCase {
-	public function test_set_directory_url_sets_value() {
-		$Resources = new LEWP\Resources\Resources( '' );
-		$Resources->set_directory_url( 'http://acme.org' );
+	public function test_sanitize_urls_removes_bad_keys() {
+		$bad_list = array(
+			'bad-thing' => 'http://acme.org/bad-thing',
+			'old-reg'   => 'http://acme.org/old-reg',
+		);
 
-		$this->assertEquals( 'http://acme.org', $Resources->get_directory_url() );
+		$good_list = array(
+			'new-reg'  => 'http://acme.org/new-reg',
+			'new-cert' => 'http://acme.org/new-cert',
+		);
+
+		$test_list = array_merge( $bad_list, $good_list );
+
+		$resources = new LEWP\Resources\Resources( json_encode( $test_list ) );
+
+		$this->assertEquals( $good_list, $resources->get_resource_urls() );
 	}
 
-	public function test_get_directory_url_gets_value() {
-		$Resources = new LEWP\Resources\Resources( '' );
-		$this->assertEquals( '', $Resources->get_directory_url() );
+	public function test_resources_are_correctly_extracted() {
+		$directory = $this->get_directory_object();
+		$resources = new LEWP\Resources\Resources( $directory->get_body() );
 
-		$Resources->set_directory_url( 'http://acme.org' );
-		$this->assertEquals( 'http://acme.org', $Resources->get_directory_url() );
+		$data = MockData::get_directory_response();
+		$data = json_decode( $data['body'], true );
 
-		$Resources2 = new LEWP\Resources\Resources( 'http://acme.org' );
-		$this->assertEquals( 'http://acme.org', $Resources2->get_directory_url() );
+		$this->assertEquals( $data, $resources->get_resource_urls() );
+	}
+
+	private function get_directory_object() {
+		$url  = 'http://acme.org/directory';
+		$args = array(
+			'body' => '',
+		);
+
+		$response = MockData::get_directory_response();
+
+		\WP_Mock::setUp();
+
+		// Mock the remote request
+		\WP_Mock::wpFunction( 'wp_remote_request', array(
+			'args'   => array(
+				$url,
+				$args,
+			),
+			'times'  => 1,
+			'return' => $response,
+		) );
+
+		$directory = new \LEWP\Request\Directory( $url );
+		$directory->send();
+
+		\WP_Mock::tearDown();
+
+		return $directory;
 	}
 }
