9492bc605a1020b122b3abe82fb1888dfb2b3d7a
John Blackbourn <johnbillion@gmail.com>
2015-10-11 06:38:04 +0100

    In-memory KeyPair storage class.

---
+++ src/Keys/Storage/Memory.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace LEWP\Keys\Storage;
+use \LEWP\Keys\Storage;
+use \LEWP\Keys\KeyPair;
+
+class Memory extends Storage {
+
+	private $keypairs = [];
+
+	public function save( KeyPair $keypair ) {
+
+		$public  = $keypair->export_public_key();
+		$private = $keypair->export_private_key();
+
+		$this->keypairs[ $keypair->get_id() ] = (object) compact( 'public', 'private' );
+
+		return true;
+
+	}
+
+	public function get( $id ) {
+
+		if ( ! array_key_exists( $id, $this->keypairs ) ) {
+			return false;
+		}
+
+		$keypair = new KeyPair( $id );
+		$keypair->read( $this->keypairs[ $id ] );
+
+		return $keypair;
+
+	}
+
+	public function delete( $id ) {
+		unset( $this->keypairs[ $id ] );
+		return true;
+	}
+
+}
