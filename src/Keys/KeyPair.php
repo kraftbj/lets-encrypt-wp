d08ad2d70e71806a78f70ec2113155420b16b882
John Blackbourn <johnbillion@gmail.com>
2015-10-12 00:33:55 +0100

    Docs!

--- src/Keys/KeyPair.php
+++ src/Keys/KeyPair.php
@@ -12,16 +12,26 @@ class KeyPair {
 	/**
 	 * Construct the object.
 	 *
-	 * @param  string     $id    The key ID.
+	 * @param  string     $id    The key pair ID.
 	 */
 	public function __construct( $id ) {
 		$this->set_id( $id );
 	}
 
+	/**
+	 * Set the key pair ID.
+	 *
+	 * @param string $id The key pair ID.
+	 */
 	public function set_id( $id ) {
 		$this->id = $id;
 	}
 
+	/**
+	 * Get the key pair ID.
+	 *
+	 * @return string The key pair ID.
+	 */
 	public function get_id() {
 		return $this->id;
 	}
@@ -29,6 +39,7 @@ class KeyPair {
 	/**
 	 * Generates a new public and private key pair.
 	 *
+	 * @param  string $passphrase The passphrase to encrypt the private key.
 	 */
 	public function generate( $passphrase ) {
 		$config = array(
@@ -48,6 +59,16 @@ class KeyPair {
 
 	}
 
+	/**
+	 * Reads a keypair from elsewhere (eg. persistent storage) and places it into the KeyPair object.
+	 *
+	 * @param array $keypair {
+	 *     An array representing the key pair.
+	 *
+	 *     @type string $public  The public key.
+	 *     @type string $private The private key.
+	 * }
+	 */
 	public function read( array $keypair ) {
 
 		$this->private_key = $keypair['private'];
@@ -77,7 +98,7 @@ class KeyPair {
 	 * Export the decrypted private key resource.
 	 *
 	 * @param  string $passphrase The passphrase to decrypt the private key.
-	 * @return resource The decrypted private key resource.
+	 * @return resource|bool The decrypted private key resource, or false on failure.
 	 */
 	public function export_private_key( $passphrase ) {
 		return openssl_pkey_get_private( $this->private_key, $passphrase );
