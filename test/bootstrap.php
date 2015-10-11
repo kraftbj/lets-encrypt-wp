d8ee0ab499cb0c7fd1b97459fd81edd4f506e764
John Blackbourn <johnbillion@gmail.com>
2015-10-11 16:49:55 +0100

    Tweaks.

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once __DIR__ . '/../vendor/autoload.php';
+require_once dirname( __DIR__ ) . '/vendor/autoload.php';
 
 class MockData {
 	static function get_directory_response() {
